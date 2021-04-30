ec4a0588de96d5b8bda98af3b5f0f6b4761e32cb
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2021-05-01 04:16:14 +0900

    feat: add shader.py for simplify the way to define shaders

---
+++ shader.py
@@ -0,0 +1,37 @@
+import sys
+from OpenGL.GL import *
+
+class Shader:
+    def __init__(self):
+        self.handle = glCreateProgram()
+    
+    def attach_shader(self, content, type, log_always=False):
+        shader = glCreateShader(type)
+        glShaderSource(shader, [content])
+        glCompileShader(shader)
+
+        status = ctypes.c_uint(GL_UNSIGNED_INT)
+        glGetShaderiv(shader, GL_COMPILE_STATUS, status)
+        if log_always or not status:
+            print(glGetShaderInfoLog(shader).decode("utf-8"), file=sys.stderr)
+            glDeleteShader(shader)
+            return False
+        
+        glAttachShader(self.handle, shader)
+        glDeleteShader(shader)
+        return True
+
+    def link(self, log_always=False):
+        glLinkProgram(self.handle)
+        status = ctypes.c_uint(GL_UNSIGNED_INT)
+        glGetProgramiv(self.handle, GL_LINK_STATUS, status)
+        if log_always or not status:
+            print(glGetProgramInfoLog(self.handle).decode("utf-8"), file=sys.stderr)
+            return False
+        return True
+    
+    def use(self):
+        glUseProgram(self.handle)
+
+    def unuse(self):
+        glUseProgram(0)
