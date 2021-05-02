440ab35111f1a1d64b9a6026925bf74c420ab87a
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2021-05-02 18:41:11 +0900

    feat: change signature of Model.setup to Model.setup(self, window: tp.Any)

--- models/model.py
+++ models/model.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
-import ctypes
 import typing as tp
 
+
+
 class Model(tp.Protocol):
-    def setup(self, resolution: tuple[int, int]) -> None: ...
+    def setup(self, window: tp.Any) -> None: ...
     def render(self) -> None: ...
 
